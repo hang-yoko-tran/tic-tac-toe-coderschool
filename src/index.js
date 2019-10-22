64e670ece2fb0d5e69cc58f7eaeacadb3c031fb2
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-22 16:46:45 +0700

    history

--- src/index.js
+++ src/index.js
@@ -6,55 +6,25 @@ const Square = props => {
     return (
       <button 
       className="square" 
-      onClick={props.onClickProp}>
+      onClick={props.onClick}>
         {props.value}
       </button>
     );
   };
 
+
+  
 const Board = props => {
-   const initialSquares = Array(9).fill(null)
-   const [squares, setSquares] = useState(initialSquares)
-   const [xIsNext, setXIsNext] = useState(true)
-   const handleClick = i => {
-    const newSquares = [...squares]
 
-    const winnerDeclared = Boolean(calculateWinner(newSquares))
-    const squareAlreadyFilled = Boolean(newSquares[i])
-    
-
-    if(!winnerDeclared && !squareAlreadyFilled){
-      newSquares[i] = xIsNext ? "X" : "O"
-      setSquares(newSquares)
-      setXIsNext(!xIsNext)
-    }
-
-    // if (winnerDeclared || squareAlreadyFilled) 
-    // return
-    // newSquares[i] = xIsNext ? "X" : "O"
-    // setSquares(newSquares)
-    // setXIsNext(!xIsNext)
-    
-   }
 
   const renderSquare = i => {
-    return <Square value={squares[i]} onClickProp={() => handleClick(i)} />;
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
-
-
-
-  const winner = calculateWinner(squares);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
 
 
   return (
     <div>
-      <div className="status">
-        {status}
-      </div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -75,14 +45,64 @@ const Board = props => {
 };
 
 const Game = props => {
+  const initialHistory = [
+    { squares: Array(9).fill(null) }
+  ];
+  const [history, setHistory] = useState(initialHistory);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0)
+
+  const handleClick = i => {
+    const slicedHistory = history.slice(0, stepNumber + 1);
+    const finalStepInSlicedHistory = slicedHistory[slicedHistory.length - 1];
+    const newSquares = [...finalStepInSlicedHistory.squares];
+    
+    const winnerDeclared = Boolean(calculateWinner(newSquares));
+    const squareAlreadyFilled = Boolean(newSquares[i]);
+    if (winnerDeclared || squareAlreadyFilled) return;
+    
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    const newStep = { squares: newSquares };
+    const newHistory = [...slicedHistory, newStep];
+    
+    setHistory(newHistory);
+    setXIsNext(!xIsNext);
+    setStepNumber(slicedHistory.length);
+  };
+  const jumpTo = step => {
+    setStepNumber(step);
+    
+    const isEvenStepNumber = step % 2 === 0;
+    setXIsNext(isEvenStepNumber);
+  };
+  const moves = history.map((step, move) => {
+    const description = Boolean(move)
+      ? `Go to move #${move}`
+      : `Go to game start`;
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  const currentStep = history[stepNumber];
+  const winner = calculateWinner(currentStep.squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          squares={currentStep.squares}
+          onClick={i => handleClick(i)}
+        />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
