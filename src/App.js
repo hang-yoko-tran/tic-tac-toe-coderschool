3527f770a740b9cb91f881e74c7044b225c7c737
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-22 17:28:35 +0700

    change to app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './index.css';
+import Game from './components/Game'
 
-import './App.css';
-
-const Square = props => {
-  const [value, setValue] = useState(null);
-
-  return (
-    <button
-      className="square"
-      onClick={() => setValue('X')} /* updated line */
-    >
-      {value} {/* updated line */}
-    </button>
-  );
-};
-
-const Board = props => {
-  const initialSquares = Array(9).fill(null); /* added line */
-  const [squares, setSquares] = useState(initialSquares); /* added line */
- return (
-   initialSquares.map(() => {
-     <Square/
-   })
- )
-
+function App () {
+  return <Game/>
 }
 
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Board/>
-    </div>
-  )}
-
-  export default App
+export default App
